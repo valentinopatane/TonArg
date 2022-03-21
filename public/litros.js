103ebc1e3984e24213592fe9623ff24e5e2defd9
Valentino Patane <valentinopatane98@gmail.com>
2022-03-21 17:53:49 -0300

    corrección calculadora de litros

--- public/litros.js
+++ public/litros.js
@@ -13,63 +13,31 @@ button.addEventListener('click', (e)=>{
         $("#superficie").val("");   
     }else{
 
-        //RENDIMIENTOS de LITROS
-        const litrosLE = 1;
-        const litrosLI = 2;
-        const litrosLP = 3;
-        const litrosRT = 4;
-        const litrosML = 5;
-        const litrosPP = 34;
-        const litrosLS = 34;
-        const litrosSF = 34;
-        const litrosEO = 34;
-        const litrosTP = 34;
-        const litrosTC = 34;
+        const litrosProd = [
+            {nombre:'Látex Exterior', litros:1},
+            {nombre:'Látex Interior', litros:2},
+            {nombre:'Látex Picasso', litros:3},
+            {nombre:'Revestimiento Texturado', litros:4},
+            {nombre:'Membrana Líquida', litros:1},
+            {nombre:'Piletas y Piscinas', litros:1},
+            {nombre:'Látex Satinado', litros:1},
+            {nombre:'Sellador Fijador', litros:1},
+            {nombre:'Enduido', litros:1},
+            {nombre:'Tropic Porcelanato', litros:1},
+            {nombre:'Tropic Cerámica', litros:1},
+        ]
 
 
-        if(product == 'Látex Exterior'){
-            var result = litrosLE * surface 
-        }
-        else if(product == 'Látex Interior'){
-            var result = litrosLI * surface 
-        }
-        else if(product == 'Látex Picasso'){
-            var result = litrosLP * surface 
-        }
-        else if(product == 'Revestimiento Texturado'){
-            var result = litrosRT * surface 
-        }
-        else if(product == 'Membrana Líquida'){
-            var result = litrosML * surface 
-        }
-        else if(product == 'Piletas y Piscinas'){
-            var result = litrosPP * surface 
-        }
-        else if(product == 'Látex Satinado'){
-            var result = litrosLS * surface 
-        }
-        else if(product == 'Sellador Fijador'){
-            var result = litrosSF * surface 
-        }
-        else if(product == 'Enduido'){
-            var result = litrosEO * surface 
-        }
-        else if(product == 'Tropic Porcelanato'){
-            var result = litrosTP * surface 
-        }
-        else if(product == 'Tropic Cerámica'){
-            var result = litrosTC * surface 
-        }
+        const obj = litrosProd.find(litros => litros.nombre === product)
 
-
-        const finalResult = Math.round(result)
+        let result = Math.round(obj.litros * surface)
 
         const nameDOM = `
         <b>${product}</b> en <b>${surface}</b> (m2)
          `
 
         const resultDOM = `
-            <p>${finalResult} lts</p>
+            <p>${result} lts</p>
         `
         const litros = document.getElementById('litros_result');
         litros.innerHTML = resultDOM;
@@ -79,9 +47,5 @@ button.addEventListener('click', (e)=>{
         $("#select_product").val("");
         $("#superficie").val("");   
 
-    
-        
     }
 });
-
-
